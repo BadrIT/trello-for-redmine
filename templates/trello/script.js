16fb97bc32438d09b5a220923bce74261a02f70c
kareem <kareem.fikry91@gmail.com>
2015-02-01 13:36:46 +0200

    initial commit

---
+++ templates/trello/script.js
@@ -0,0 +1,115 @@
+angular.module('app')
+
+.controller('DashboardCtrl', ['$scope', '$timeout',
+	function($scope, $timeout) {
+		$scope.gridsterOptions = {
+			margins: [20, 20],
+			columns: 3,
+			draggable: {
+				handle: 'h3'
+			}
+		};
+
+		$scope.widgets= [];
+
+		$scope.clear = function() {
+			$scope.widgets = [];
+		};
+
+		$scope.addWidget = function() {
+			$scope.widgets.push({
+				name: "Userstory #" + $scope.widgets.length,
+				sizeX: 1,
+				sizeY: 1,
+                cards: [{
+                    name: 'my card'
+                },{
+                    name: 'another one'
+                }]
+			});
+		};
+        
+        $scope.sortableTemplates = {
+            connectWith: '.connectedSortable',
+            dropOnEmpty: true
+        };
+	}
+])
+
+.controller('CustomWidgetCtrl', ['$scope', '$modal',
+	function($scope, $modal) {
+
+		$scope.remove = function(widget) {
+			$scope.widgets.splice($scope.widgets.indexOf(widget), 1);
+		};
+
+		$scope.openSettings = function(widget) {
+			$modal.open({
+				scope: $scope,
+				templateUrl: 'templates/trello/widget_settings.html',
+				controller: 'WidgetSettingsCtrl',
+				resolve: {
+					widget: function() {
+						return widget;
+					}
+				}
+			});
+		};
+
+	}
+])
+
+.controller('WidgetSettingsCtrl', ['$scope', '$timeout', '$rootScope', '$modalInstance', 'widget',
+	function($scope, $timeout, $rootScope, $modalInstance, widget) {
+		$scope.widget = widget;
+
+		$scope.form = {
+			name: widget.name,
+			sizeX: widget.sizeX,
+			sizeY: widget.sizeY,
+			col: widget.col,
+			row: widget.row
+		};
+
+		$scope.sizeOptions = [{
+			id: '1',
+			name: '1'
+		}, {
+			id: '2',
+			name: '2'
+		}, {
+			id: '3',
+			name: '3'
+		}, {
+			id: '4',
+			name: '4'
+		}];
+
+		$scope.dismiss = function() {
+			$modalInstance.dismiss();
+		};
+
+		$scope.remove = function() {
+			$scope.widgets.splice($scope.widgets.indexOf(widget), 1);
+			$modalInstance.close();
+		};
+
+		$scope.submit = function() {
+			angular.extend(widget, $scope.form);
+
+			$modalInstance.close(widget);
+		};
+
+	}
+])
+
+// helper code
+.filter('object2Array', function() {
+	return function(input) {
+		var out = [];
+		for (i in input) {
+			out.push(input[i]);
+		}
+		return out;
+	}
+});
