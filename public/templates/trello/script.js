f1bc085e387cd890dd2fbf2ddc0ab4bd139be4c4
Hazem Hagrass <hazem.hagrass@gmail.com>
2015-02-04 14:02:54 +0200

    Loading when moving card in the same list

--- public/templates/trello/script.js
+++ public/templates/trello/script.js
@@ -1,166 +1,175 @@
 angular.module('trelloRedmine')
 
 .controller('DashboardCtrl', ['$scope', '$timeout', '$modal', '$http',
-	function($scope, $timeout, $modal, $http) {
-		$scope.gridsterOptions = {
-			margins: [20, 20],
-			columns: 3,
-			draggable: {
-				handle: '.box-header'
-			},
-			swapping: true,
-			resizable: {
-	           handles: ['s']
-	        }
-		};
+ function ($scope, $timeout, $modal, $http) {
+        $scope.gridsterOptions = {
+            margins: [20, 20],
+            columns: 3,
+            draggable: {
+                handle: '.box-header'
+            },
+            swapping: true,
+            resizable: {
+                handles: ['s']
+            }
+        };
 
-		$scope.widgets= [];
+        $scope.widgets = [];
 
-		$scope.clear = function() {
-			$scope.widgets = [];
-		};
+        $scope.clear = function () {
+            $scope.widgets = [];
+        };
 
-		$scope.addCard = function(widget) {
-			$modal.open({
-				scope: $scope,
-				templateUrl: 'templates/trello/add_card.html',
-				controller: 'AddCardCtrl',
-				resolve: {
-					widget: function() {
-						return widget;
-					}
-				}
-			});
-		}
+        $scope.addCard = function (widget) {
+            $modal.open({
+                scope: $scope,
+                templateUrl: 'templates/trello/add_card.html',
+                controller: 'AddCardCtrl',
+                resolve: {
+                    widget: function () {
+                        return widget;
+                    }
+                }
+            });
+        }
 
-		$http.get('/dashboard').success(function(data, status){
-			$scope.widgets = data.dashboard.widgets;
-		}).error(function(data, status){
-			$scope.widgets.push({
-				name: "Error #" + $scope.widgets.length,
-				sizeX: 1,
-				sizeY: 1,
+        $http.get('/dashboard').success(function (data, status) {
+            $scope.widgets = data.dashboard.widgets;
+        }).error(function (data, status) {
+            $scope.widgets.push({
+                name: "Error #" + $scope.widgets.length,
+                sizeX: 1,
+                sizeY: 1,
                 cards: [{
                     name: 'my card'
-                },{
+                }, {
                     name: 'another one'
                 }]
-			});
-		});
+            });
+        });
 
-		$scope.addWidget = function() {
-			$scope.widgets.push({
-				name: "Userstory #" + $scope.widgets.length,
-				sizeX: 1,
-				sizeY: 1,
+        $scope.addWidget = function () {
+            $scope.widgets.push({
+                name: "Userstory #" + $scope.widgets.length,
+                sizeX: 1,
+                sizeY: 1,
                 cards: [{
-                	thumb: "http://cssdeck.com/uploads/media/items/2/2v3VhAp.png",
+                    thumb: "http://cssdeck.com/uploads/media/items/2/2v3VhAp.png",
                     name: 'my card'
-                },{
-                	thumb: "http://cssdeck.com/uploads/media/items/6/6f3nXse.png",
+                }, {
+                    thumb: "http://cssdeck.com/uploads/media/items/6/6f3nXse.png",
                     name: 'another one'
                 }]
-			});
-		};
-        
+            });
+        };
+
         $scope.sortableTemplates = {
             connectWith: '.connectedSortable',
-            dropOnEmpty: true
+            dropOnEmpty: true,
+            stop: function (event, ui) {
+                $(ui.item).find("#overlay").show();
+                setTimeout(function () {
+                    $(ui.item).find("#overlay").hide();
+                }, 1000);
+            }
         };
-	}
+ }
 ])
 
 .controller('CustomWidgetCtrl', ['$scope', '$modal',
-	function($scope, $modal) {
+ function ($scope, $modal) {
 
-		$scope.remove = function(widget) {
-			$scope.widgets.splice($scope.widgets.indexOf(widget), 1);
-		};
+        $scope.remove = function (widget) {
+            $scope.widgets.splice($scope.widgets.indexOf(widget), 1);
+        };
 
-		$scope.openSettings = function(widget) {
-			$modal.open({
-				scope: $scope,
-				templateUrl: 'templates/trello/widget_settings.html',
-				controller: 'WidgetSettingsCtrl',
-				resolve: {
-					widget: function() {
-						return widget;
-					}
-				}
-			});
-		};
+        $scope.openSettings = function (widget) {
+            $modal.open({
+                scope: $scope,
+                templateUrl: 'templates/trello/widget_settings.html',
+                controller: 'WidgetSettingsCtrl',
+                resolve: {
+                    widget: function () {
+                        return widget;
+                    }
+                }
+            });
+        };
 
-	}
+ }
 ])
 
 .controller('WidgetSettingsCtrl', ['$scope', '$timeout', '$rootScope', '$modalInstance', 'widget',
-	function($scope, $timeout, $rootScope, $modalInstance, widget) {
-		$scope.widget = widget;
+ function ($scope, $timeout, $rootScope, $modalInstance, widget) {
+        $scope.widget = widget;
 
-		$scope.form = {
-			name: widget.name,
-			sizeX: widget.sizeX,
-			sizeY: widget.sizeY,
-			col: widget.col,
-			row: widget.row
-		};
+        $scope.form = {
+            name: widget.name,
+            sizeX: widget.sizeX,
+            sizeY: widget.sizeY,
+            col: widget.col,
+            row: widget.row
+        };
 
-		$scope.sizeOptions = [{
-			id: '1',
-			name: '1'
-		}, {
-			id: '2',
-			name: '2'
-		}, {
-			id: '3',
-			name: '3'
-		}, {
-			id: '4',
-			name: '4'
-		}];
+        $scope.sizeOptions = [{
+            id: '1',
+            name: '1'
+  }, {
+            id: '2',
+            name: '2'
+  }, {
+            id: '3',
+            name: '3'
+  }, {
+            id: '4',
+            name: '4'
+  }];
 
-		$scope.dismiss = function() {
-			$modalInstance.dismiss();
-		};
+        $scope.dismiss = function () {
+            $modalInstance.dismiss();
+        };
 
-		$scope.remove = function() {
-			$scope.widgets.splice($scope.widgets.indexOf(widget), 1);
-			$modalInstance.close();
-		};
+        $scope.remove = function () {
+            $scope.widgets.splice($scope.widgets.indexOf(widget), 1);
+            $modalInstance.close();
+        };
 
-		$scope.submit = function() {
-			angular.extend(widget, $scope.form);
+        $scope.submit = function () {
+            angular.extend(widget, $scope.form);
 
-			$modalInstance.close(widget);
-		};
+            $modalInstance.close(widget);
+        };
 
-	}
+ }
 ])
 
 .controller('AddCardCtrl', ['$scope', '$timeout', '$rootScope', '$modalInstance', 'widget',
-	function($scope, $timeout, $rootScope, $modalInstance, widget) {
-		$scope.widget = widget;
-		$scope.card = {name: '', thumb: ''};
+ function ($scope, $timeout, $rootScope, $modalInstance, widget) {
+        $scope.widget = widget;
+        $scope.card = {
+            name: '',
+            thumb: ''
+        };
 
-		$scope.dismiss = function() {
-			$modalInstance.dismiss();
-		};
+        $scope.dismiss = function () {
+            $modalInstance.dismiss();
+        };
 
-		$scope.submit = function() {
-			widget.cards.push($scope.card);
-			$modalInstance.close(widget);
-		};
+        $scope.submit = function () {
+            widget.cards.push($scope.card);
+            $modalInstance.close(widget);
+        };
 
-	}
+ }
 ])
 
 // helper code
-.filter('object2Array', function() {
-	return function(input) {
-		var out = [];
-		for (i in input) {
-			out.push(input[i]);
-		}
-		return out;
-	}
+.filter('object2Array', function () {
+    return function (input) {
+        var out = [];
+        for (i in input) {
+            out.push(input[i]);
+        }
+        return out;
+    }
 });
