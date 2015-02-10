cd303f1d0dae6051c83f5b5dc7dd42d786f7457e
kareem <kareem.fikry91@gmail.com>
2015-02-10 16:59:37 +0200

    built card edit

--- public/templates/trello/script.js
+++ public/templates/trello/script.js
@@ -199,6 +199,37 @@ angular.module('trelloRedmine')
 
     }
 ])
+
+// .directive('contenteditable', function() {
+//   return {
+//     require: 'ngModel',
+//     link: function(scope, elm, attrs, ctrl) {
+//       // view -> model
+//       elm.bind('blur', function() {
+//         scope.$apply(function() {
+//           ctrl.$setViewValue(elm.html());
+//         });
+//       });
+ 
+//       // model -> view
+//       ctrl.render = function(value) {
+//         elm.html(value);
+//       };
+ 
+//       // load init value from DOM
+//       ctrl.$setViewValue(elm.html());
+    
+//      elm.bind('keypress', function(e) {
+//          alert('someone press esc');
+//          if (e.charCode === 27) scope.$apply(function() {
+//           ctrl.$setViewValue(elm.html());
+//          });
+//         });
+    
+//     }
+      
+//   };
+// })
 // helper code
 .filter('object2Array', function() {
     return function(input) {
